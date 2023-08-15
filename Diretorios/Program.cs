927d5d63fc1c22c3b3054509c77c49d43d5cfdc2
Leandro Guimaraes <leandroguim@hotmail.com>
2023-08-15 12:51:17 -0300

    Add project files.

---
+++ Diretorios/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+string path = @"c:\temp\myfolder";
+
+try
+{
+    IEnumerable<string> folders =  Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories);
+    Console.WriteLine("FOLDERS:");
+    foreach (string s in folders)
+    {
+        Console.WriteLine(s); 
+    }
+}
+catch(IOException e)
+{
+    Console.WriteLine("An error occurred");
+    Console.WriteLine(e.Message);
+}
